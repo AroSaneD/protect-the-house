ea08d9ba35a6cb3f6474c1cba8cc054fdd2c2986
Arūnas Seniucas <Arnas Seniucas>
2018-01-02 00:40:07 +0200

    Moved house to center of map

--- src/app/model/ABuilding.ts
+++ src/app/model/ABuilding.ts
@@ -9,7 +9,7 @@ export class ABuilding implements IBuilding, IDamageable, IDrawable {
     color: string;
     hp: number;
 
-    position: Vector = new Vector(50, 50);
+    position: Vector = new Vector(375, 250);
     size: Vector = new Vector(50, 50);
 
     constructor(private context: CanvasRenderingContext2D) {
@@ -23,7 +23,7 @@ export class ABuilding implements IBuilding, IDamageable, IDrawable {
     draw() {
         new UsingCanvas(this.context, (context) => {
             context.fillStyle = "lightgreen";
-            context.fillRect(this.position.x, this.position.y, this.size.x, this.size.y); // Todo: Use position and width parameters
+            context.fillRect(this.position.x - this.size.x / 2, this.position.y - this.size.y / 2, this.size.x, this.size.y); // Todo: Use position and width parameters
         });
     }
 
